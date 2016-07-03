3fb2aac16ea6abe189bde362cee7939188a9b421
marcevrard <marc.evrard@gmail.com>
2016-07-03 13:26:40 +0200

    Team added

--- code/tweet.py
+++ code/tweet.py
@@ -1,3 +1,11 @@
+#!/bin/env python3
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#   Usage:  python3 ~/scripts_tat/run_lf0_max_from_numpy.py show
+#   From:   /Users/mev/Dropbox/SHARE_UBUNTU/lf0/lf0
+#
+
 import datetime
 
 class tweet:
