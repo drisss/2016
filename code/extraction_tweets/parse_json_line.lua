df4c74ec289f5b9514eea33905091822e4506886
Vincent Letard <vincent.letard@limsi.fr>
2016-07-03 11:29:39 +0200

    Fix extraction: GMT+2

--- code/extraction_tweets/parse_json_line.lua
+++ code/extraction_tweets/parse_json_line.lua
@@ -4,6 +4,8 @@ local JSON  = require "JSON"
 local utils = require "toolbox"
 local date  = require "date"
 
+local GMT = +2
+
 local counter = 1
 for line in io.stdin:lines() do
 --  io.stderr:write(counter.."\n")
@@ -14,7 +16,7 @@ for line in io.stdin:lines() do
     assert(not processed_text:match("\t"))
     assert(not processed_text:match("\n"))
     -- id time text retweet_count lang
-    local processed_time = date.gmtime(plain.timestamp_ms:sub(1,-4))
+    local processed_time = date.gmtime(tonumber(plain.timestamp_ms:sub(1,-4))+(GMT * 3600))
     print(string.format("%s\t%s\t%s\t%s\t%s", plain.id_str, processed_time, processed_text, plain.retweet_count, plain.lang))
   end
 end
